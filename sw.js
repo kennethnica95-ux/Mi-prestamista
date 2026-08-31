183dbc163974e2698a1008c15d056d6150db5b6d
kennethnica95-ux <kennethnica95@gmail.com>
2026-08-31 09:47:52 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-const CACHE="mi-prestamista-v1";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='mp2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
